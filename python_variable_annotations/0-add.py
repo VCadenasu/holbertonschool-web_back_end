6b4232acf1446a8482830d4f5613a54f6f08dbe6
Victor Cadenas <vlcadenasu@hotmail.com>
2024-01-24 22:35:33 -0500

    add file 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+Type-annoted function add that takes two
+floats variables and return their sum as float
+"""
+def add(a: float, b: float) -> float:
+    """Returns the result"""
+    return a+b
