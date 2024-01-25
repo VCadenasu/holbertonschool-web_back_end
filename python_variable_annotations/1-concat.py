f75b2df1c6951a132fff4ecd00d7e16805158e91
Victor Cadenas <vlcadenasu@hotmail.com>
2024-01-24 22:47:31 -0500

    add file 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/bin/usr/env python3
+"""
+A type-annoted function concat that
+takes two strings as arguments and returns it concatenated
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """Return a concatenated string"""
+    return f"{a}{b}"
