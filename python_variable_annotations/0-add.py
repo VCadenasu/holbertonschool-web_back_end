1510881b364e6de39548fddbdc4c19915888ba87
Victor Cadenas <vlcadenasu@hotmail.com>
2024-01-24 22:37:13 -0500

    fix file 0 add blank line 7

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,6 +4,8 @@
 Type-annoted function add that takes two
 floats variables and return their sum as float
 """
+
+
 def add(a: float, b: float) -> float:
     """Returns the result"""
     return a+b
