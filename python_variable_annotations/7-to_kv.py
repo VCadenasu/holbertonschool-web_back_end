078049fbf058fc27b3ec120b138474288f8202d8
Victor Cadenas <vlcadenasu@hotmail.com>
2024-01-25 21:48:17 -0500

    add file 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function to_kv taking
+k string and v int or float as arguments
+return a tuple.
+"""
+
+
+from typing import Union, Tuple
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple with k as first element and square v"""
+    return k, float(v ** 2)
+    
