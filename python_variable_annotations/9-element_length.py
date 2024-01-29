dc628edc515569c82399e68fc35375f910e52e86
Victor Cadenas <vlcadenasu@hotmail.com>
2024-01-28 22:12:35 -0500

    add file 9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Annotate the a function parameter
+and return values with the appropriate types
+"""
+
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Calculate the length of each element in list"""
+    return [(i, len(i)) for i in lst]
