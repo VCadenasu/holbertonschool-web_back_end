e527b2fd5d9669e5a8b08eaf27c660fa497003e9
Victor Cadenas <vlcadenasu@hotmail.com>
2024-02-18 00:43:44 -0500

    fix file 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -6,7 +6,7 @@ export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
 
-    return [task, task2]
+    return [task, task2];
   }
 
   return [task, task2];
