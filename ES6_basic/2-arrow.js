8ff5323070a1877f868c5dd9fbfc3dcc907a7ba6
Victor Cadenas <vlcadenasu@hotmail.com>
2024-02-18 00:54:33 -0500

    add file 2

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,5 +1,5 @@
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'union Square'];
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
     const self = this;
     this.addNeighborhood = (newNeighborhood) => {
