a52491c1f62f80fb2da05d75548d9b06655c7d5b
Victor Cadenas <vlcadenasu@hotmail.com>
2024-02-18 19:42:08 -0500

    add file 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
     const NewArray = [];
-    NewArray = appendString + idx;
+    NewArray.push(appendString + idx);
   }
 
   return NewArray;
