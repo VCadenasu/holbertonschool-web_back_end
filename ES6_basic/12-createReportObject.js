8f6a0e2793bcdeb01dc617605040a5985960309f
Victor Cadenas <vlcadenasu@hotmail.com>
2024-02-18 20:06:10 -0500

    add file 12

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const obs = {
+    allEmployees: {...employeesList},
+    getNumberOfDepartments(employeesList) {
+        return Object.keys(employeesList).length;
+    },
+  };
+
+  return obs;
+}
