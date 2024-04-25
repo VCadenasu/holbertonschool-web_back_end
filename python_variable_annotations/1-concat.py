e6166b467a506f3fdbf804bbfb7c22e106f3f74d
Victor Cadenas <vlcadenasu@hotmail.com>
2024-04-25 09:09:28 -0500

    ADD: File 1-concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Type annotated function concat
+str1 and str2 as arguments
+Returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    #concate the string
+    str3 = str1 + str2
+    return str3
