fbbce8fbeb93113201532cedfd4eb6da9c2c51ed
Victor Cadenas <vlcadenasu@hotmail.com>
2024-04-25 09:39:49 -0500

    ADD: File 2-floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+A type annotated function floor 
+Takes a float n as argument
+Returns the floor of the float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the float as int"""
+    return (math.floor(n))
--- python_variable_annotations/README.md
+++ python_variable_annotations/README.md
@@ -1,1 +1,3 @@
 0-add: Wrtiring a type annotation variable for the arguments and result.
+1-concat: Gives a function wich returns a concatenated string.
+2-floor: Give a function wich returns the int value of a float variable.
