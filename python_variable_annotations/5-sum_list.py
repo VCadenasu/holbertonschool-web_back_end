04d5da7e11216a13ec628da7cde22dc7facc8de3
Victor Cadenas <vlcadenasu@hotmail.com>
2024-04-26 22:52:23 -0500

    FIX: add import library and type annotation in argument.

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,9 +5,10 @@ A type annotated function sum_list
 Takes input_list of floats as argument
 Returns the sum as float
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Realize the sum and return float"""
     sum = 0
     for val in input_list:
