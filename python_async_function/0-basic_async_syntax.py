bc45ccb1f15ebc24e51b5a8678a9e4d22760ded4
Victor Cadenas <vlcadenasu@hotmail.com>
2024-04-29 10:45:40 -0500

    FIX: giving type annotations in the function.

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """Make a randon number within 0 and max_delay"""
     random_delay = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
