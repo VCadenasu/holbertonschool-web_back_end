1ac49eb0098f51938226e4381f4ce090d995e6ee
Victor Cadenas <vlcadenasu@hotmail.com>
2024-04-29 09:11:19 -0500

    ADD: File 7-to_kev

---
+++ python_variable_annotations/7-to_kev.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Type annotated function to_kev
+Takes a string k and int or float v as arguments
+Returns a tuple with k as first element and v square
+"""
+from typing import Union, Tuple
+
+
+def to_kev(k: str, v: Union[float, int]) -> Tuple[str, float]:
+    """Return the tuple"""
+    return (k, float(v ** 2))
