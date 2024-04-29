318181562dbc0e83292726847cb9efd4b06e3b6d
Victor Cadenas <vlcadenasu@hotmail.com>
2024-04-29 12:11:56 -0500

    FIX: removing part of definition in function

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -10,7 +10,7 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Measures the total execution time for wait_n"""
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
