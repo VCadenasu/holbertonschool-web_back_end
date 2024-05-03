44a50f9bbb5c5368c8c80df0adcdc9be93259dbf
Victor Cadenas <vlcadenasu@hotmail.com>
2024-05-02 19:45:36 -0500

    ADD: File 8-make_multiplier and 8-main

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,7 +5,12 @@ Type annotated function make_multiplier
 Takes a float multiplier as argument
 Returns a multiply float by multiplier
 """
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> float:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return the result as float"""
+    def inner_function(f: float) -> float:
+        """Return the inner_function result"""
+        return float(f * multiplier)
+    return (inner_function)
