45fb984ce918430db846078845924b06835f54c4
VCadenasu <vlcadenasu@gmail.com>
2024-05-15 21:26:39 -0500

    FIX: variable in condition in file 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,12 +3,12 @@ Export a function with boolen values
 */
 
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    const task = false;
+    const task2 = true;
 
     if (trueOrFalse) {
-        let task = false;
-        let task2 = true;
+        const task = false;
+        const task2 = true;
 
         return [task, task2]
     }
